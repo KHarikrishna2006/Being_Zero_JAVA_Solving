b91f99db54f9ab5839c698b4eef1719854ff4455
Kummari HariKrishna <harikrishnakummari7@gmail.com>
2026-02-27 21:09:48 +0530

    Leetcode 144
    Given the root of a binary tree, return the preorder traversal of its nodes' values.

---
+++ TREE TRAVERSEL IN  JAVA/Binary Tree Preorder Traversal.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> a=new ArrayList<>();
+        if(root==null) return a;
+        Stack<TreeNode> st=new Stack<>();
+        st.push(root);
+         while(!st.isEmpty()){
+            TreeNode k=st.pop();
+            a.add(k.val);
+        if(k.right!=null) st.push(k.right);
+        if(k.left!=null) st.push(k.left);
+    }
+        return a;
+    }
+   
+}
