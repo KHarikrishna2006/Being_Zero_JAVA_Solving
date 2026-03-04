01db58f092224a02329172d09f1d80266c10ad16
Kummari HariKrishna <harikrishnakummari7@gmail.com>
2026-03-04 12:02:01 +0530

    Leetcode 1582
    Given an m x n binary matrix mat, return the number of special positions in mat.
    
    A position (i, j) is called special if mat[i][j] == 1 and all other elements in row i and column j are 0 (rows and columns are 0-indexed).

---
+++ Leetcode/Special Positions in a Binary Matrix.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int c=0;
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                boolean b=true;
+                if(mat[i][j]==1){
+                    for (int k = 0; k < mat.length; k++) {
+                        if (k != i && mat[k][j] == 1) {
+                            b = false;
+                            break;
+                        }
+                    }
+                    if (b) {
+                        for (int k = 0; k < mat[0].length; k++) {
+                            if (k != j && mat[i][k] == 1) { 
+                                b = false;
+                                break;
+                            }
+                        }
+                    }
+                    if(b) c++;
+                }
+            }
+        }
+        return c;
+    }
+}
